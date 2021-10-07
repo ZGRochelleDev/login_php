e91abd141b9450d3a3fdcf645a45aa41668d6268
ZGRochelle <zgrochelle@gmail.com>
2021-10-07 14:56:41 -0400

    Update conn.php

--- Web_App_Assignment/php/conn.php
+++ Web_App_Assignment/php/conn.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-	$db = new PDO('mysql:host=localhost;dbname=central','drupaluser', 'zgrochelle');
+    $db = new PDO('mysql:host=localhost;dbname=central','drupaluser', 'zgrochelle');
     #$db = new PDO('mysql:host=$servername;dbname=$myDB',$username, $password);
 } catch (PDOException $e) {
     echo $e->getMessage()."<br>";
